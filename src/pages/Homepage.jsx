5df9978f8927fad8054499458ce38c04dc504014
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-24 23:24:21 +0300

    added hero section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,21 @@
-import React from "react";
 function Homepage() {
   return (
     <main>
       <section>
-        <h1>Find the right freelancer for your project</h1>
-        <p>Connect with skilled freelancers and get your work done.</p>
+        <div>
+          <h1>Find the right freelancer for your project</h1>
+
+          <p>Connect with skilled freelancers and get your work done.</p>
+
+          <div>
+            <input
+              type="text"
+              placeholder="What service are you looking for?"
+            />
+
+            <button>Search</button>
+          </div>
+        </div>
       </section>
 
       <section>
