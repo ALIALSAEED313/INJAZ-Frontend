13b9a7677a7ac109119c122addb0db3586535d7b
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-24 23:08:50 +0300

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,31 @@
-import { Link } from 'react-router'
-import { useAuth } from '../context/AuthContext'
+import { Link } from "react-router";
+import { useAuth } from "../context/AuthContext";
 
 function Navbar() {
-  const { logout, user} = useAuth()
+  const { logout, user } = useAuth();
+
   return (
     <nav>
-      {user 
-      ? 
-      (<>
-      <button onClick={logout}>Sign Out</button>
-      </>) : 
-      (<>
-        <Link to='/sign-up'>Sign Up</Link>
-        <Link to='/sign-in'>Sign In</Link>
-      </>)}
+      <Link to="/">FreelanceHub</Link>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+
+        {user ? (
+          <>
+            <Link to="/dashboard">Dashboard</Link>
+            <button onClick={logout}>Sign Out</button>
+          </>
+        ) : (
+          <>
+            <Link to="/sign-up">Sign Up</Link>
+            <Link to="/sign-in">Sign In</Link>
+          </>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
