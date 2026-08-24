71f07a0919dc9e2f8d162c51fd073578533e8a75
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-24 23:13:17 +0300

    added   footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router";
+
 function Footer() {
   return (
     <footer>
-      <h2>FreelanceHub</h2>
-      <p>Connect with skilled freelancers and get your work done.</p>
+      <div>
+        <h2>FreelanceHub</h2>
+        <p>Connect with skilled freelancers and get your work done.</p>
+      </div>
+
+      <div>
+        <h3>Quick Links</h3>
+
+        <Link to="/">Home</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/privacy">Privacy Policy</Link>
+        <Link to="/terms">Terms & Conditions</Link>
+      </div>
     </footer>
   );
 }
