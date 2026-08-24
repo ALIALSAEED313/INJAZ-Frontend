bf093519ce68480ba146044283d7dfab8c834d00
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-24 23:11:16 +0300

    added basic  footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <h2>FreelanceHub</h2>
+      <p>Connect with skilled freelancers and get your work done.</p>
+    </footer>
+  );
+}
+
+export default Footer;
