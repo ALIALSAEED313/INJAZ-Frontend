183d78ddd6805487c14c2beb725a5d9068a9fa66
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-25 00:00:01 +0300

    added searchby catogary service

--- src/services/serviceService.js
+++ src/services/serviceService.js
@@ -1,10 +1,17 @@
 import axios from "axios";
+
 export async function searchServices(search) {
   const response = await axios.get(
     `http://localhost:3000/services?search=${encodeURIComponent(search)}`,
   );
 
-  console.log("SEARCH RESPONSE:", response.data);
+  return response.data;
+}
+
+export async function getServicesByCategory(category) {
+  const response = await axios.get(
+    `http://localhost:3000/services?category=${encodeURIComponent(category)}`,
+  );
 
   return response.data;
 }
