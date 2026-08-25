506dc2c5ad1645236f774935add9500e307820f4
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-25 15:10:29 +0300

    created page structure

---
+++ src/pages/PrivacyPolicyPage.jsx
@@ -0,0 +1,14 @@
+function PrivacyPolicyPage() {
+  return (
+    <main>
+      <h1>Privacy Policy</h1>
+
+      <p>
+        At INJAZ, we respect your privacy and are committed to protecting your
+        personal information.
+      </p>
+    </main>
+  );
+}
+
+export default PrivacyPolicyPage;
