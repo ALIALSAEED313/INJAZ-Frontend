489f6f2fa22e0d157b0c9b4bf74b0ff390a7bff3
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-25 15:11:32 +0300

    user information collection

--- src/pages/PrivacyPolicyPage.jsx
+++ src/pages/PrivacyPolicyPage.jsx
@@ -7,6 +7,13 @@ function PrivacyPolicyPage() {
         At INJAZ, we respect your privacy and are committed to protecting your
         personal information.
       </p>
+
+      <h2>Information We Collect</h2>
+
+      <p>
+        We may collect information such as your username, email address, profile
+        information, and information related to services and orders.
+      </p>
     </main>
   );
 }
