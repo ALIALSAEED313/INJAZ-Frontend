09a04ce4f5e4e4b93d080645bd32ce7a58529f20
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-25 14:35:15 +0300

    add service state

--- src/pages/ServiceDetailsPage.jsx
+++ src/pages/ServiceDetailsPage.jsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 function ServiceDetailsPage() {
   const { id } = useParams();
 
+  const [service, setService] = useState(null);
+
   return (
     <main>
       <h1>Service Details</h1>
