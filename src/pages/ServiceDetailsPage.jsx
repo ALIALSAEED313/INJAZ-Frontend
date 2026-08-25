20039f562801392d97308c2e410c9417fd274936
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-25 14:35:57 +0300

    fetch service details

--- src/pages/ServiceDetailsPage.jsx
+++ src/pages/ServiceDetailsPage.jsx
@@ -3,13 +3,23 @@ import { useParams } from "react-router-dom";
 
 function ServiceDetailsPage() {
   const { id } = useParams();
-
   const [service, setService] = useState(null);
+
+  useEffect(() => {
+    async function getService() {
+      const response = await fetch(`http://localhost:3000/services/${id}`);
+
+      const data = await response.json();
+
+      setService(data);
+    }
+
+    getService();
+  }, [id]);
 
   return (
     <main>
       <h1>Service Details</h1>
-      <p>Service ID: {id}</p>
     </main>
   );
 }
