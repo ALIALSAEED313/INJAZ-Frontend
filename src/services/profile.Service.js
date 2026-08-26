8e73359b84f7cce1aee5dcc300d6cf3d3fd0fe96
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-26 12:57:21 +0300

    feat: create profile service

---
+++ src/services/profile.Service.js
@@ -0,0 +1,31 @@
+import api from './api'
+
+
+async function getProfile(userId){
+    const response = await api.get(`/profile/${userId}`)
+    return response.data
+}
+
+async function getMyProfile(){
+    const response = await api.get('/profile/me')
+    return response.data
+}
+
+async function updateProfile(formData){
+    const response = await api.put('/profile', formData)
+    return response.data
+}
+
+async function deleteProfile(){
+    const response = await api.delete('/profile')
+    return response.data
+}
+
+
+export {
+    getProfile,
+    getMyProfile,
+    updateProfile,
+    deleteProfile
+}
+
